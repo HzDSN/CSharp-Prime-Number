f19a958e629bf62a671490e5dc9f722a8977c947
dongs <dongs@DESKTOP-8R6AIJ0>
2016-11-29 21:57:32 +0800

    添加项目文件。

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, f;
+            for(i=2;i<=100;i++)
+            {
+                f = 0;
+                for(j=2;j<=i-1;j++)
+                {
+                    if(i%j==0)
+                    {
+                        f = 1;
+                        break;
+                    }
+                }
+                if(f==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
